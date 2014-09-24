7d27bcc1ba0fbedec070b4a866f912a1b954e1d6
Emily Hoehne <emilyhoehne@theglutconomist.home>
2014-09-23 22:37:24 -0600

    Finishes Step 1

---
+++ EJHListViewController.m
@@ -0,0 +1,37 @@
+//
+//  EJHListViewController.m
+//  TheLastTime
+//
+//  Created by Emily Hoehne on 9/23/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import "EJHListViewController.h"
+
+@interface EJHListViewController ()
+
+@end
+
+@implementation EJHListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
--- TheLastTime/AppDelegate.m
+++ TheLastTime/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "EJHListViewController.h"
 
 @implementation AppDelegate
 
@@ -14,6 +15,12 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    EJHListViewController *newViewController = [EJHListViewController new];
+    
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:newViewController];
+    self.window.rootViewController = navController;
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
