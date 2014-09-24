cf6135971a708515d3cb21bb5d676720b79bee2c
Emily Hoehne <emilyhoehne@theglutconomist.home>
2014-09-23 22:40:52 -0600

    Finishes step 3

--- EJHListViewController.m
+++ EJHListViewController.m
@@ -9,6 +9,8 @@
 #import "EJHListViewController.h"
 
 @interface EJHListViewController ()
+@property (strong, nonatomic) IBOutlet UITextField *textField;
+@property (strong, nonatomic) IBOutlet UILabel *label;
 
 @end
 
@@ -22,6 +24,8 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)button:(id)sender {
 }
 
 /*
